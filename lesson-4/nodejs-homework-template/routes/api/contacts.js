acae34d95e0c08e7b31927fd19a5c9ec477e4c04
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-01-19 19:22:55 +0200

    add lesson 4

---
+++ lesson-4/nodejs-homework-template/routes/api/contacts.js
@@ -0,0 +1,25 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.get('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.post('/', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.delete('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+router.patch('/:contactId', async (req, res, next) => {
+  res.json({ message: 'template message' })
+})
+
+module.exports = router
