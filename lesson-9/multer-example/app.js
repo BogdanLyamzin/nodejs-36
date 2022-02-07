bcec430b12c7773a9647c25397c58040e6a63e4e
MandorSavall <bogdan.lyamzin.d@gmail.com>
2022-02-07 20:17:32 +0200

    add empty folder

---
+++ lesson-9/multer-example/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+
+const app = express();
+
+app.use(cors());
+
+const products = [];
+
+const upload = multer({dest: })
+
+app.post("/api/products", async(req, res, next)=> {
+
+})
